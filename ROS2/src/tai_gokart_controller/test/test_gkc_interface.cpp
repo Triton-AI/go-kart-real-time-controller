13641ec65b771197dcd14d2b72a91efe5fd4bc9c
Haoru Xue <hxue@ucsd.edu>
2022-02-07 02:45:55 -0800

    add dummy test

---
+++ ROS2/src/tai_gokart_controller/test/test_gkc_interface.cpp
@@ -0,0 +1,10 @@
+/**
+ * @file test_gkc_interface.cpp
+ * @author Haoru Xue (haoru.xue@autoware.org)
+ * @brief
+ * @version 0.1
+ * @date 2022-02-07
+ *
+ * @copyright Copyright 2022 Triton AI
+ *
+ */
