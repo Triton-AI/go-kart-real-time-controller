ef2bb2334c4a3ea699b0b1ffce8d862685c12053
Haoru Xue <hxue@ucsd.edu>
2022-02-06 17:37:35 -0800

    populate launch file and param file

---
+++ ROS2/src/tai_gokart_controller/launch/tai_gokart_controller.launch.py
@@ -0,0 +1,24 @@
+# Copyright 2022 Triton AI
+
+import os
+
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+
+    gkc_config = os.path.join(
+        get_package_share_directory('tai_gokart_controller'),
+        'params',
+        'tai_gokart_controller_config.yaml'
+        )
+    return LaunchDescription([
+        Node(
+            package='tai_gokart_controller',
+            executable='tai_gokart_controller_node',
+            name='tai_gokart_controller_node',
+            parameters=[gkc_config]
+        ),
+    ])
